7333a4343440c77ef2d88f75354bd9802f753e8e
Dmitriy <div4ik@live.ru>
2023-04-12 16:39:19 +0500

    Поменяли вывод

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Привет мир!");
+        System.out.println("lalala");
         System.out.println("lalala");
     }
 }
