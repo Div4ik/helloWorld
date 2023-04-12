a6cf669ac2c1a6f0bd7286acd9f86c4a773a158d
Dmitriy <div4ik@live.ru>
2023-04-12 22:40:40 +0500

    Решение задачи "Сколько дней составит доставка карты". прошу любить и жаловать "китайский код написанный на коленке"

---
+++ deliveryDaysCalculation/src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        int distance = 60;
+        System.out.println(DeliveryDaysCalculation(distance));
+
+
+
+    }
+
+    public static String DeliveryDaysCalculation (int distance){
+        int deliveryDays = 0;
+        String result = "";
+
+        if(distance < 60){
+            deliveryDays = distance / 20 + 1;
+        }
+
+        if (deliveryDays == 0) {
+            result = "Доставка не возможна";
+        } else {
+            result = "На расстоянии " + distance + " км доставка составит " + deliveryDays + " " + days(deliveryDays);
+        }
+
+        return result;
+
+
+    }
+
+    public static String days (int days) {
+        switch(days){
+            case 1:
+                return "день";
+            case 2:
+            case 3:
+                return "дня";
+            default:
+                return "Ошибка";
+
+
+        }
+    }
+}
