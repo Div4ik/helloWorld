592eae00ab9b17e2a9bd807eec53c239963759ca
Dmitriy <div4ik@live.ru>
2023-04-19 18:12:53 +0500

    Book ДЗ

---
+++ Main/src/Book/Main.java
@@ -0,0 +1,19 @@
+package Book;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Author tolstoy = new Author("Лев", "Толстой");
+        Author pushkin = new Author("Александр", "Пушкин");
+
+        Book warAndPeace = new Book("Война и мир", 1851, tolstoy);
+        Book fishermanAndFish = new Book("Сказка о рыбаке и рыбке", 1835, pushkin);
+
+        warAndPeace.setYearOfPublication(1861);
+
+        System.out.println(pushkin);
+        System.out.println(pushkin.toString());
+        System.out.println(warAndPeace);
+        System.out.println(fishermanAndFish);
+    }
+}
